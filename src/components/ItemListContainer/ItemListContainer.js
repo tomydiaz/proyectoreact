5db9827b88a59b7e4cfea2c62991c6226da6eb83
tomydiaz <tomydiaz739@gmail.com>
2021-03-22 21:34:44 -0300

    .

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,13 +2,38 @@ import { useEffect, useState } from "react";
 import productList from "../../mocks/productList.js";
 import ItemList from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
+import { getFirestore } from "../../firebase/index";
 
 const ItemListContainer = () => {
   const [productos, setProductos] = useState([]);
 
+  const [prod, setProd] = useState([]);
+
   const { productoId } = useParams();
 
+  const db = getFirestore();
+
+  const itemCollection = db.collection("productos");
+  itemCollection.get().then((value) => {
+    let aux = value.docs.map((element) => {
+      return element.data();
+    });
+    console.log(aux);
+    setProd(aux);
+  });
+
   useEffect(() => {
+    const db = getFirestore();
+
+    const itemCollection = db.collection("productos");
+    itemCollection.get().then((value) => {
+      let aux = value.docs.map((element) => {
+        return element.data();
+      });
+      console.log(aux);
+      setProd(aux);
+    });
+
     var productListFiltrada = productList.filter((producto) => {
       if (producto.idCategoria.toString() === productoId) {
         return producto;
@@ -29,6 +54,7 @@ const ItemListContainer = () => {
   return (
     <>
       <ItemList productos={productos} />
+      <ItemList productos={prod} />
     </>
   );
 };
